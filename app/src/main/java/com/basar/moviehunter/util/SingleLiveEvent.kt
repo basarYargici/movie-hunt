288be08b9712ec23bfe86eb64b4e20b001eed8d6
basarYargici <ibrahimbyargici@gmail.com>
2022-07-09 02:40:49 +0300

    SingleLiveEvent created to notify observer only once

---
+++ app/src/main/java/com/basar/moviehunter/util/SingleLiveEvent.kt
@@ -0,0 +1,40 @@
+package com.basar.moviehunter.util
+
+import androidx.annotation.MainThread
+import androidx.annotation.Nullable
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MutableLiveData<T> {
+
+    constructor() : super()
+
+    constructor(value: T) : super(value)
+
+    private val mPending: AtomicBoolean = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner) { t ->
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        }
+    }
+
+    @MainThread
+    override fun setValue(@Nullable t: T?) {
+        mPending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
