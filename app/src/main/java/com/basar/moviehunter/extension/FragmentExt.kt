b898cbc3d3e129643a8548ec3d9385f144063e82
basarYargici <ibrahimbyargici@gmail.com>
2022-07-12 18:09:43 +0300

    download image and asking permission implemented

---
+++ app/src/main/java/com/basar/moviehunter/extension/FragmentExt.kt
@@ -0,0 +1,13 @@
+package com.basar.moviehunter.extension
+
+import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.requestPermissionList(request: ActivityResultLauncher<Array<String>>, permissions: Array<String>)
+= request.launch(permissions)
+
+fun Fragment.isAllPermissionsGranted(permissions: Array<String>) = permissions.all {
+    ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+}
--- app/src/main/java/com/basar/moviehunter/ui/homepage/moviedetail/MovieDetailFragment.kt
+++ app/src/main/java/com/basar/moviehunter/ui/homepage/moviedetail/MovieDetailFragment.kt
@@ -1,28 +1,41 @@
 package com.basar.moviehunter.ui.homepage.moviedetail
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
 import com.basar.moviehunter.base.BaseFragment
 import com.basar.moviehunter.databinding.FragmentMovieDetailBinding
-import com.basar.moviehunter.extension.getImageEndpoint
-import com.basar.moviehunter.extension.observe
-import com.basar.moviehunter.extension.setImageBitmap
+import com.basar.moviehunter.extension.*
 import com.basar.moviehunter.ui.view.movielist.MovieListAdapter
 import com.basar.moviehunter.util.Listener
 import com.basar.moviehunter.util.Receiver
 import com.basar.moviehunter.util.categoryMapper
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 
 @AndroidEntryPoint
 class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding>(), Receiver, Listener {
     private val viewModel: MovieDetailViewModel by viewModels()
     val args: MovieDetailFragmentArgs by navArgs()
+    private var isPermissionGranted = false
+
+    private lateinit var permissionsRequest: ActivityResultLauncher<Array<String>>
+
+    companion object {
+        private val PERMISSIONS = arrayOf(WRITE_EXTERNAL_STORAGE)
+    }
 
     override fun inflateLayout(
         inflater: LayoutInflater,
@@ -35,6 +48,7 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding>(), Receiver
         viewModel.initVM(movieId)
         setReceiver()
         setListeners()
+        permissionsRequest = getPermissionsRequest()
     }
 
     override fun setReceiver() {
@@ -79,6 +93,45 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding>(), Receiver
     override fun setListeners() {
         binding.btnShare.setOnClickListener {
             shareMessage(viewModel.movieDetail.value.toString())
+        }
+        binding.btnDownload.setOnClickListener {
+            requestPermissionList(permissionsRequest, PERMISSIONS)
+        }
+    }
+
+    private fun getPermissionsRequest() =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (isAllPermissionsGranted(PERMISSIONS)) {             //extension function
+                downloadImage(
+                    "https://img.freepik.com/premium-vector/warrior-logo-gaming_43623-412.jpg?w=2000",
+                    "warrior"
+                )
+            } else {
+                Toast.makeText(
+                    context,
+                    "Permission should be accepted to download the image",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+    private fun downloadImage(url: String, fileName: String) {
+        try {
+            var downloadManager = activity?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val imageLink = Uri.parse(url)
+            var req = DownloadManager.Request(imageLink)
+            req.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
+                .setMimeType("image/jpeg")
+                .setAllowedOverRoaming(false)
+                .setTitle(fileName)
+                .setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_PICTURES, File.separator + fileName + ".jpg"
+                )
+            downloadManager.enqueue(req)
+            Toast.makeText(context, "downloaded", Toast.LENGTH_SHORT).show()
+
+        } catch (e: Exception) {
+            Toast.makeText(context, "failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
