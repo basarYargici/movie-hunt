07439256461ab0720dbb3ce2ba4611d1426cf604
basarYargici <ibrahimbyargici@gmail.com>
2022-07-31 05:23:56 +0300

    - Color update on views

--- app/src/main/java/com/basar/moviehunter/ui/view/movielist/MovieListView.kt
+++ app/src/main/java/com/basar/moviehunter/ui/view/movielist/MovieListView.kt
@@ -3,16 +3,16 @@ package com.basar.moviehunter.ui.view.movielist
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.basar.moviehunter.data.model.MovieResponse
 import com.basar.moviehunter.databinding.ViewMovieListBinding
 import com.basar.moviehunter.domain.uimodel.MovieListUI
-import com.google.android.material.card.MaterialCardView
 
 class MovieListView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : MaterialCardView(context, attrs, defStyleAttr) {
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
     private val binding: ViewMovieListBinding =
         ViewMovieListBinding.inflate(
             LayoutInflater.from(context),
